7b44568c31a70697c8aaff7f293457507a807cd6
N-Sridhar <nsridharbtech@gmail.com>
2020-08-02 23:57:40 +0530

    firebase import

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCiQj8-fy4V4S71Wi9y27LDlFXhFcVeLj8',
